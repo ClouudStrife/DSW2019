05238ba545c03c182cd44c4c9e8ec76073a89a9f
CAIO <caio.b.santos@hotmail.com>
2019-04-21 23:14:41 -0300

    LocacaoDAO

---
+++ src/DSWprojectOne/src/java/src/org/dsw/model/Locacao.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package src.org.dsw.model;
+
+/**
+ *
+ * @author Guga Bot
+ */
+public class Locacao {
+    private String cpfCliente;
+    private String cnpjLocadora;
+    private String dateHour;
+    
+    public String getCpfCliente(){
+        return this.cpfCliente;
+    }
+    
+    public void setCpfCliente(String cpfCliente){
+        this.cpfCliente = cpfCliente;
+    }
+    
+    public String getCnpjLocadora(){
+        return this.cnpjLocadora;
+    }
+    
+    public void setCnpjLocadora(String cnpjLocadora){
+        this.cnpjLocadora = cnpjLocadora;
+    }
+    
+    public String getDateHour(){
+        return this.dateHour;
+    }
+    
+    public void setDateHour(String dateHour){
+        this.dateHour = dateHour;
+    }
+}
